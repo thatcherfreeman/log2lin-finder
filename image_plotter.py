058db45685d14f4bb1578b03df48da53ca7bdf79
Thatcher Freeman <thatcher@gastinel.com>
2023-06-04 14:03:44 -0700

    added clipping point and linear regression tools to image plotter

--- image_plotter.py
+++ image_plotter.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+
 os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
 import cv2
 import argparse
@@ -9,6 +10,7 @@ import matplotlib.pyplot as plt
 def convert_to_hsv(image: np.ndarray) -> np.ndarray:
     assert len(image.shape) == 3
     out = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    out[:, :, 0] /= 360.0
     print(np.max(out[:, :, 0]))
     return out
 
@@ -22,44 +24,41 @@ def open_image(image_fn: str) -> np.ndarray:
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
 
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("x_file", type=str, help="Image to read on the x-axis.")
+    parser.add_argument("y_file", type=str, help="Image displayed on the y-axis")
     parser.add_argument(
-        'x_file',
-        type=str,
-        help='Image to read on the x-axis.'
+        "--xlog",
+        action="store_true",
+        help="plot x-axis as log plot",
     )
     parser.add_argument(
-        'y_file',
-        type=str,
-        help='Image displayed on the y-axis'
+        "--ylog",
+        action="store_true",
+        help="plot y-axis as log plot",
     )
     parser.add_argument(
-        '--xlog',
-        action='store_true',
-        help='plot x-axis as log plot',
+        "--num_samples", type=int, default=10000, help="number of points to plot."
+    )
+    parser.add_argument("--swap", action="store_true", help="swap x and y axes.")
+    parser.add_argument(
+        "--mode",
+        choices=["huesat", "codevalue", "satsat"],
+        default="codevalue",
+        help="Choose the kind of plot.",
     )
     parser.add_argument(
-        '--ylog',
-        action='store_true',
-        help='plot y-axis as log plot',
+        "--clipping_point",
+        type=float,
+        default=None,
+        help="Pixel values above this number will be ignored.",
     )
     parser.add_argument(
-        '--num_samples',
-        type=int,
-        default=10000,
-        help='number of points to plot.'
-    )
-    parser.add_argument(
-        '--swap',
-        action='store_true',
-        help='swap x and y axes.'
-    )
-    parser.add_argument(
-        '--mode',
-        choices=['huesat', 'codevalue', 'satsat'],
-        default='codevalue',
-        help='Choose the kind of plot.'
+        "--linear_regression",
+        action="store_true",
+        help="set this flag to run a linear regression on the samples.",
     )
 
     args = parser.parse_args()
@@ -72,7 +71,9 @@ def main():
     print("Y image shape: ", y_img.shape)
     if x_img.shape != y_img.shape:
         print(f"Shapes mismatched, resizing Y image to {x_img.shape}")
-        y_img = cv2.resize(y_img, (x_img.shape[1], x_img.shape[0]), interpolation=cv2.INTER_AREA)
+        y_img = cv2.resize(
+            y_img, (x_img.shape[1], x_img.shape[0]), interpolation=cv2.INTER_AREA
+        )
         print(y_img.shape)
 
     assert x_img.shape == y_img.shape
@@ -86,53 +87,106 @@ def main():
 
     sampled_coords = np.random.choice(h, num_samples), np.random.choice(w, num_samples)
 
-    if args.mode in ('huesat', 'satsat'):
+    if args.mode in ("huesat", "satsat"):
         x_img = convert_to_hsv(x_img)
         y_img = convert_to_hsv(y_img)
 
-    x_samples = x_img[sampled_coords[0], sampled_coords[1]] # num_samples, c
+    x_samples = x_img[sampled_coords[0], sampled_coords[1]]  # num_samples, c
     y_samples = y_img[sampled_coords[0], sampled_coords[1]]
 
+    sample_mask = np.ones(num_samples, dtype=bool)
+    if args.clipping_point is not None:
+        sample_mask = sample_mask & (np.max(x_samples, axis=1) < args.clipping_point)
+        sample_mask = sample_mask & (np.max(y_samples, axis=1) < args.clipping_point)
+        x_samples = x_samples[sample_mask]
+        y_samples = y_samples[sample_mask]
 
-    if args.mode == 'codevalue':
-        colors = ['red', 'green', 'blue']
+    if args.mode == "codevalue":
+        colors = ["red", "green", "blue"]
         for i in range(c):
             if i < len(colors):
                 color = colors[i]
             else:
-                color = np.random.rand(3,)
-            plt.scatter(x_samples[:, i], y_samples[:, i], marker='.', alpha=0.1, color=color)
+                color = np.random.rand(3)
+            plt.scatter(
+                x_samples[:, i], y_samples[:, i], marker=".", alpha=0.1, color=color
+            )
         max_val = np.max([x_img, y_img])
+        if args.clipping_point is not None:
+            max_val = np.min([max_val, args.clipping_point])
         min_val = np.min([x_img, y_img])
         samples = np.linspace(min_val, max_val, 50)
         plt.plot(samples, samples)
+        if args.linear_regression:
+            params, _, _, _ = np.linalg.lstsq(
+                np.stack(
+                    [x_samples.flatten(), np.ones_like(x_samples.flatten())], axis=1
+                ),
+                y_samples.flatten(),
+                rcond=None,
+            )
+            print(f"Linear Regression: {params[0]}x + {params[1]}")
+            plt.plot(samples, (params[0] * samples + params[1]))
 
-    elif args.mode == 'huesat':
+    elif args.mode == "huesat":
         fig, ax1 = plt.subplots()
         sample_mask = x_samples[:, 1] > 0.1
         x_samples = x_samples[sample_mask]
         y_samples = y_samples[sample_mask]
 
-        plt.scatter(x_samples[:, 0], (y_samples[:, 0] - x_samples[:, 0]) / 360, color='red', marker='.', alpha=0.1, label='Hue')
-        plt.scatter(x_samples[:, 0], y_samples[:, 1] / x_samples[:, 1], color='green', marker='.', alpha=0.1, label='Saturation')
+        plt.scatter(
+            x_samples[:, 0] * 360.0,
+            (y_samples[:, 0] - x_samples[:, 0]),
+            color="red",
+            marker=".",
+            alpha=0.1,
+            label="Hue",
+        )
+        plt.scatter(
+            x_samples[:, 0] * 360.0,
+            y_samples[:, 1] / x_samples[:, 1],
+            color="green",
+            marker=".",
+            alpha=0.1,
+            label="Saturation",
+        )
+        samples = np.linspace(0.0, 1.0, 50)
         plt.hlines(1.0, 0.0, 360.0)
         plt.hlines(0.0, 0.0, 360.0)
         plt.legend()
 
-    elif args.mode == 'satsat':
-        plt.scatter(x_samples[:, 1], y_samples[:, 1], color='green', marker='.', alpha=0.1, label='Saturation')
+    elif args.mode == "satsat":
+        x_samples = x_samples[:, 1]
+        y_samples = y_samples[:, 1]
+        plt.scatter(
+            x_samples,
+            y_samples,
+            color="green",
+            marker=".",
+            alpha=0.1,
+            label="Saturation",
+        )
         max_val = np.max([x_img[:, 1], y_img[:, 1]])
         min_val = np.min([x_img[:, 1], y_img[:, 1]])
         min_val, max_val = 0.0, 1.0
         samples = np.linspace(min_val, max_val, 50)
         plt.plot(samples, samples)
+        if args.linear_regression:
+            params, _, _, _ = np.linalg.lstsq(
+                np.expand_dims(x_samples.flatten(), axis=1),
+                y_samples.flatten(),
+                rcond=None,
+            )
+            print(f"Linear Regression: {params[0]}x")
+            plt.plot(samples, (params[0] * samples))
 
     if args.xlog:
-        plt.xscale('log')
+        plt.xscale("log")
     if args.ylog:
-        plt.yscale('log')
+        plt.yscale("log")
     plt.show()
-    print("MSE: ", np.mean((x_img - y_img)**2))
+
+    print("MSE: ", np.mean((x_img - y_img) ** 2))
 
 
 if __name__ == "__main__":
